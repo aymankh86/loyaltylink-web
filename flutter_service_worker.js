49d5323c9e9c7942e00be8f28805da1c35238444
Ayman Khalil <ayman.khalil33@gmail.com>
2024-02-26 14:18:10 +0300

    new build

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,8 +4,8 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "cc08eb5ca2b4e9ca0f1ae1f94ce80b56",
-"index.html": "ce091a3da050776550c85457f1bbc7a0",
-"/": "ce091a3da050776550c85457f1bbc7a0",
+"index.html": "12f5a7773a5a13b38ab39b1082097bc9",
+"/": "12f5a7773a5a13b38ab39b1082097bc9",
 "main.dart.js": "6727aa045e5815428b8f84ef88c416de",
 "flutter.js": "7d69e653079438abfbb24b82a655b0a4",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
